c2718730732989445081121a31a68a31754d20fd
KsUdal <udalcova.v2.0@yandex.ru>
2022-05-04 12:24:50 +0300

    Makefile improved, directory stb_image removed, nothing works

--- work/main.c
+++ work/main.c
@@ -1,21 +1,19 @@
-#include <iostream>
-
 #include "stb_image.h"
 #include "stb_image_write.h"
-
+#include "stb_image_resize.h"
 #include <strings.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 // Макроподстановка, меняющая p(i, j) на  idata[(i)+(j)*iw]
 
-#define p(i, j)  idata[(i)+(j)*iw]
+//#define p(i, j)  idata[(i)+(j)*iw] условное обозначение, которое может и не понадобиться
 
 int main() {
 
     // строка, путь к файлу
-    char* inputPath = "~/Pictures/input.png";
-    int iw, ih, n;
+    char*inputPath = "~/work/hampster.png";
+    int iw, ih, n; //ширина, высота и количество цветовых каналов
 
     // Загружаем изображение, чтобы получить информацию о ширине, высоте и цветовом канале
     unsigned char *idata = stbi_load(inputPath, &iw, &ih, &n, 0);
@@ -23,7 +21,9 @@ int main() {
         printf("ERROR: can't read file %s\n", inputPath );
         return 1;
     }
-    /* Демонстрационные опыты для семинара */
+
+    /*
+    //Демонстрационные опыты для семинара
     char * pixel = idata;
     // char red = pixel[0];
     // char blue = pixel[1];
@@ -45,7 +45,7 @@ int main() {
     for (int s = -d; s <= d; s++) {
         for (int t = -d; t<= d; t++) {
             p1 = p(i + s, j+ t);
-            // мы считаем пороговое значение по красному каналу = 20. 
+            // мы считаем пороговое значение по красному каналу = 20.
             // Параметрами можно играть самостоятельно!
             if (abs (p1[0] - p[0] ) < 20 ) {
                 ...
@@ -53,12 +53,13 @@ int main() {
         }
     }
 
-
-    char* outputPath = "~/Pictures/output.png";
+    char* outputPath = "~/work/output.png";
     // записываем картинку
     stbi_write_png(outputPath, ow, oh, n, odata, 0);
+    */
 
+    printf("Изображение размера %d в высоту и %d в ширину с количеством каналов %d считано", ih, iw, n);
     stbi_image_free(idata);
-    stbi_image_free(odata);
+    stbi_image_free(idata);
     return 0;
 }
