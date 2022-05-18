303d40aeb5ae3f0420c9c9a3fa0fcf8f87ebe43a
KsUdal <udalcova.v2.0@yandex.ru>
2022-05-18 16:04:16 +0300

    Still want to make it show n-channel picture

--- work/main.c
+++ work/main.c
@@ -123,9 +123,9 @@ int main() {
 
     // записываем картинку
     int one = 1; int zero = 0;
-    stbi_write_png(outputPath, iw, ih, n, odata, zero);
+    stbi_write_png(outputPath, iw, ih, n, odata, 1);
     //stbi_image_write(outputPath, iw, ih, 2, MyImage, 0);
-    printf("Изображение размера %d в высоту и %d в ширину с количеством каналов %d считано", ih, iw, n);
+    //printf("Изображение размера %d в высоту и %d в ширину с количеством каналов %d считано", ih, iw, n);
     stbi_image_free(idata);
     stbi_image_free(newIm);
     stbi_image_free(MyImage);
