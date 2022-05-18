0ada31d1270764de5b51e81bc82697ca1008989a
KsUdal <udalcova.v2.0@yandex.ru>
2022-05-18 08:53:25 +0300

    New image of max added

--- work/main.c
+++ work/main.c
@@ -15,10 +15,10 @@
 int main() {
 
     // строка, путь к файлу
-    //char*inputPath = "hampster.png";
+    char*inputPath = "hampster.png";
     //char*inputPath = "tooth.png";
     //char*inputPath = "arm_break.png";
-    char*inputPath = "Arrow_head.png";
+    //char*inputPath = "Arrow_head.png";
     int iw, ih, n; //ширина, высота и количество цветовых каналов
 
     // Загружаем изображение, чтобы получить информацию о ширине, высоте и цветовом канале
@@ -32,6 +32,7 @@ int main() {
     k = 0;
     unsigned char* MyImage = (unsigned char*)malloc(ih*iw*sizeof(unsigned char));
     unsigned char* odata = (unsigned char*)malloc(ih*iw*sizeof(unsigned char));
+    unsigned char* newIm = (unsigned char*)malloc(ih*iw*sizeof(unsigned char));
     for (i = 0; i < ih*iw*n; i = i + n) {
         MyImage[k] = 0.299*idata[i] + 0.587*idata[i + 1] + 0.114*idata[i + 2];
         k = k + 1;
@@ -53,20 +54,13 @@ int main() {
             //Roberts
             x = MyImage[iw*(i-1)+(j-1)] - MyImage[iw*i+j];
             y = MyImage[iw*(i-1)+j] - MyImage[iw*i+(j+1)];
-
-/*
-            //Sobel apgrade
-            x = 3*MyImage[iw*(i-1)+(j-1)] + 10*MyImage[iw*i+(j-1)] + 3*MyImage[iw*(i+1)+(j-1)];
-            x = x - 3*MyImage[iw*(i-1)+(j+1)] - 10*MyImage[iw*i+(j+1)] - 3*MyImage[iw*(i+1)+(j+1)];
-            y = 3*MyImage[iw*(i-1)+(j-1)] + 10*MyImage[iw*(i-1)+j] + 3*MyImage[iw*(i-1)+(j+1)];
-            y = y - 3*MyImage[iw*(i+1)+(j-1)] - 10*MyImage[iw*(i+1)+j] - 3*MyImage[iw*(i+1)+(j+1)];
-*/
 /*
             //casual Sobel
             x = MyImage[iw*(i-1)+(j-1)] + 2*MyImage[iw*i+(j-1)] + MyImage[iw*(i+1)+(j-1)];
             x = x - MyImage[iw*(i-1)+(j+1)] - 2*MyImage[iw*i+(j+1)] - MyImage[iw*(i+1)+(j+1)];
             y = MyImage[iw*(i-1)+(j-1)] + 2*MyImage[iw*(i-1)+j] + MyImage[iw*(i-1)+(j+1)];
             y = y - MyImage[iw*(i+1)+(j-1)] - 2*MyImage[iw*(i+1)+j] - MyImage[iw*(i+1)+(j+1)];
+
 */
 /*
             //just an experiment
@@ -88,13 +82,25 @@ int main() {
         }
     }
 
-    char* outputPath = "output_arrow_head.png";
+    //third image craetion
+    for (i = 0; i < ih*iw; i++) {
+        if (MyImage[i] > odata[i]) {
+            newIm[i] = MyImage[i];
+        }
+        else {
+            newIm[i] = odata[i];
+        }
+    }
+
+    //char* outputPath = "output_arrow_head.png";
     //char* outputPath = "output_arm_break.png";
     //char* outputPath = "output_hamster.png";
     //char* outputPath = "output_tooth.png";
+    char* outputPath = "output.png";
+
     // записываем картинку
     int one = 1; int zero = 0;
-    stbi_write_png(outputPath, iw, ih, one, odata, zero);
+    stbi_write_png(outputPath, iw, ih, one, newIm, zero);
     //stbi_image_write(outputPath, iw, ih, 2, MyImage, 0);
     //printf("Изображение размера %d в высоту и %d в ширину с количеством каналов %d считано", ih, iw, n);
     stbi_image_free(idata);
