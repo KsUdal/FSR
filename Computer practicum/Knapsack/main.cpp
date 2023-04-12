579c8e5417b19798453ecb16883d8e9fae337d2b
KsUdal <ksudal_1@DESKTOP-6VCFJ9P.localdomain>
2023-04-12 17:40:00 +0300

    minizinc

---
+++ Computer practicum/Knapsack/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n, capacity, i;
+    cin >> n >> capacity;
+    vector<int> profit(n);
+    vector<int> size(n);
+    for (i = 0; i < n; i++) {
+        cin >> profit[i] >> size[i];
+    }
+
+    ofstream data;
+    data.open("data.dzn");
+
+    data << "capacity = " << capacity << ";" << endl;
+    data << "n = " << n << ";" << endl;
+    data << "profit = [";
+    for (i = 0; i < n-1; i++) data << profit[i] << ",";
+    data << profit[n-1] << "];" << endl;
+    data << "size = [";
+    for (i = 0; i < n-1; i++) data << size[i] << ",";
+    data << size[n-1] << "];";
+
+    data.close();
+
+    // $ minizinc --solver Gecode solver.mzn data.dzn (for console)
+    // how to make it here?
+
+    //MznSolver slv(std::cout,std::cerr);
+    //vector<std::string> options({"--stdlib-dir", "--solver", "org.gecode.gecode"});
+    //slv.run(options, modelString, "minizinc", "solver.mzn");
+
+    return 0;
+}
+
